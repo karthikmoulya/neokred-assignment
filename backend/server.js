76d7034e1936f60513d6dcb196ca5d1aed32f390
karthikmoulya <karthikmoulya1995@gmail.com>
2024-11-29 19:01:53 +0530

    minor fix

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { marked } from 'marked';
 import cors from 'cors';
-import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
 app.use(cors());
-
-app.use(
-  express.static(
-    path.join(__dirname, 'frontend/dist')
-  )
-);
-
-app.get('*', (req, res) => {
-  res.sendFile(
-    path.join(
-      __dirname,
-      'frontend/dist',
-      'index.html'
-    )
-  );
-});
 
 app.post('/convert', (req, res) => {
   const { markdown } = req.body;
